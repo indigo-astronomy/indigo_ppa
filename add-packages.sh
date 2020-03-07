73cc749e6a8f983218cfdb992a60584dba3f8a6b
Peter Polakovic <peter.polakovic@gmail.com>
2020-03-07 23:01:42 +0100

    INDIGO 2.0-114 added

--- add-packages.sh
+++ add-packages.sh
@@ -3,7 +3,6 @@ aptly repo create -config=aptly.conf -distribution=indigo -component=main indigo
 aptly repo add -config=aptly.conf indigo-release ppa/pool/*/*/*
 aptly repo add -config=aptly.conf indigo-release $*
 aptly repo show -config=aptly.conf -with-packages indigo-release
-aptly publish drop -config=aptly.conf -force-drop indigo
 aptly publish repo -config=aptly.conf indigo-release
 cp -Rf public/* ppa
 rm -rf db pool public
